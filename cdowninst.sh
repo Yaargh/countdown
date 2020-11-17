859691dd5904996b955134c9d73d5de604a50613
Yaargh <corry.clinton@protonmail.com>
2020-11-17 05:40:21 -0500

    Typos

--- cdowninst.sh
+++ cdowninst.sh
@@ -1,9 +1,9 @@
 #! bin/bash
 
-# Script to install countdown plasmoid. It updates your system, installs git if
-# not already there, then downloads and installs the app. If you happn to
-# already have 'foobartemp' directory, just change the temp directory in the
-# script to something else, you weirdo, because it deletes the directory.
+# Script to install countdown plasmoid. It updates your system, installs Git if
+# not already there, then downloads and installs the app. If you happen to
+# already have 'foobarfoobartemp' directory, just change the temp directory in
+# the script to something else, you weirdo, because it deletes the directory.
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install git -y
